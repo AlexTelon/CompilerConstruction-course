6f0a56ff5227bef70ca84a54cae412c372fe704e
Alex Telon <alete471@student.liu.se>
2015-12-04 17:12:29 +0100

    lab5 almost done

--- remaining/optimize.cc
+++ remaining/optimize.cc
@@ -95,6 +95,12 @@ void ast_stmt_list::optimize()
 void ast_expr_list::optimize()
 {
     /* Your code here */
+    if (preceding != NULL) {
+        preceding->optimize();
+    }
+    if (last_expr != NULL) {
+        last_expr->optimize();
+    }
 }
 
 
@@ -102,6 +108,12 @@ void ast_expr_list::optimize()
 void ast_elsif_list::optimize()
 {
     /* Your code here */
+    if (preceding != NULL) {
+        preceding->optimize();
+    }
+    if (last_elsif != NULL) {
+        last_elsif->optimize();
+    }
 }
 
 
@@ -116,8 +128,8 @@ void ast_id::optimize()
 void ast_indexed::optimize()
 {
     /* Your code here */
+	index->optimize();
 }
-
 
 
 /* This convenience method is used to apply constant folding to all
@@ -126,49 +138,175 @@ void ast_indexed::optimize()
 ast_expression *ast_optimizer::fold_constants(ast_expression *node)
 {
     /* Your code here */
+	if (is_binop(node)) { // kinda unneccesary
+		ast_binaryoperation *binop_node = node->get_ast_binaryoperation();
+		if (binop_node == NULL) {
+			return NULL;
+		}
+		sym_index left_type = binop_node->left->type;
+		sym_index right_type = binop_node->right->type;
+		long left_int, right_int;
+		double left_real, right_real;
+
+
+		// need to be int or real to be foldable TODO check ast_id, sida 114
+		if ((left_type != integer_type || left_type != real_type) &&
+			(right_type != integer_type || right_type != real_type)) {
+
+			if (left_type == integer_type) {
+				if(binop_node->left->get_ast_integer() == NULL){
+					return NULL;
+				}
+				left_int = binop_node->left->get_ast_integer()->value;
+				left_real = (double)left_int;
+			} else {
+				if(binop_node->left->get_ast_real() == NULL){
+					return NULL;
+				}
+				left_real = binop_node->left->get_ast_real()->value;
+			}
+
+			if (right_type == integer_type) {
+				if(binop_node->right->get_ast_integer() == NULL){
+					return NULL;
+				}
+				right_int = binop_node->right->get_ast_integer()->value;
+			    right_real = (double)left_int;
+			} else {
+				if(binop_node->right->get_ast_real() == NULL){
+					return NULL;
+				}
+				right_real = binop_node->right->get_ast_real()->value;
+			}
+			if (left_type == integer_type && right_type == integer_type) {
+				long retval;
+				switch (binop_node->tag) {
+				case AST_ADD: retval = left_int + right_int; break;
+				case AST_SUB: retval = left_int - right_int; break;
+				case AST_OR: retval = left_int || right_int; break;
+				case AST_AND: retval = left_int && right_int; break;
+				case AST_MULT: retval = left_int * right_int; break;
+				case AST_DIVIDE:  return new ast_real(binop_node->pos, left_int / right_int); // TODO if right is 0??
+				case AST_IDIV: retval = left_int / right_int; break;
+				case AST_MOD: retval = left_int % right_int; break;
+				default:
+					type_error(binop_node->pos) << "what is this? You suck!" << endl;
+				}
+				return new ast_integer(binop_node->pos, retval);				
+			} else {
+				double retval;
+				switch (binop_node->tag) {
+				case AST_ADD: retval = left_real + right_real; break;
+				case AST_SUB: retval = left_real - right_real; break;
+
+				case AST_OR:
+				case AST_MOD: 
+				case AST_AND:  type_error(binop_node->pos) << "Invalid operation,"
+													 << " to OR, AND or MOD with reals in optimization."
+													 << endl; break;
+
+				case AST_MULT: retval = left_real * right_real; break;
+				case AST_DIVIDE:  return new ast_real(binop_node->pos, left_real / right_real); // TODO if right is 0??
+				case AST_IDIV: type_error(binop_node->pos) << "Invalid operation, IDIV with reals in optimization."
+													 << endl; break;
+
+				default:
+					type_error(binop_node->pos) << "what is this? You suck!" << endl;
+				}
+				return new ast_real(binop_node->pos, retval);				
+			}
+
+
+		// type_error(node->pos) << "batman left type " << left->type << endl; //node->left << endl;
+		// type_error(node->pos) << "batman right " << right << endl;
+
+		}
+
+
+	}
     return NULL;
 }
+
 
 /* All the binary operations should already have been detected in their parent
    nodes, so we don't need to do anything at all here. */
 void ast_add::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
 }
 
 void ast_sub::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_mult::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_divide::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_or::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_and::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_idiv::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_mod::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 
@@ -177,21 +315,41 @@ void ast_mod::optimize()
 void ast_equal::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_notequal::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_lessthan::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 void ast_greaterthan::optimize()
 {
     /* Your code here */
+	ast_expression* new_ast_left = optimizer->fold_constants(this->left);
+	ast_expression* new_ast_right = optimizer->fold_constants(this->right);
+	if (new_ast_left != NULL) this->left = new_ast_left;
+	if (new_ast_right != NULL) this->right = new_ast_right;
+
 }
 
 
@@ -201,52 +359,91 @@ void ast_greaterthan::optimize()
 void ast_procedurecall::optimize()
 {
     /* Your code here */
+	if (parameter_list != NULL) {
+		parameter_list->optimize();
+	}
 }
 
 
 void ast_assign::optimize()
 {
     /* Your code here */
+	lhs->optimize();
+	rhs->optimize();
+
+	ast_expression* new_ast_right = optimizer->fold_constants(this->rhs);
+	if (new_ast_right != NULL) this->rhs = new_ast_right;
+	
 }
 
 
 void ast_while::optimize()
 {
     /* Your code here */
+	condition->optimize();
+	body->optimize();
+
+	ast_expression* new_ast_cond = optimizer->fold_constants(this->condition);
+	if (new_ast_cond != NULL) this->condition = new_ast_cond;
+
 }
 
 
 void ast_if::optimize()
 {
     /* Your code here */
+	condition->optimize();
+	body->optimize();
+
+	ast_expression* new_ast_cond = optimizer->fold_constants(this->condition);
+	if (new_ast_cond != NULL) this->condition = new_ast_cond;
+
+	if (elsif_list != NULL) {
+		elsif_list->optimize();
+	}
+
+	if (else_body != NULL) {
+		else_body->optimize();
+	}
 }
 
 
 void ast_return::optimize()
 {
     /* Your code here */
+	value->optimize();
+
+	ast_expression* new_ast_value = optimizer->fold_constants(this->value);
+	if (new_ast_value != NULL) this->value = new_ast_value;
 }
 
 
 void ast_functioncall::optimize()
 {
     /* Your code here */
+	if (parameter_list != NULL) {
+		parameter_list->optimize();
+	}
 }
 
 void ast_uminus::optimize()
 {
     /* Your code here */
+	expr->optimize();
 }
 
 void ast_not::optimize()
 {
     /* Your code here */
+	expr->optimize();
 }
 
 
 void ast_elsif::optimize()
 {
     /* Your code here */
+	condition->optimize();
+	body->optimize();
 }
 
 
@@ -254,11 +451,13 @@ void ast_elsif::optimize()
 void ast_integer::optimize()
 {
     /* Your code here */
+	// do nothing.
 }
 
 void ast_real::optimize()
 {
     /* Your code here */
+	// do nothing.
 }
 
 /* Note: See the comment in fold_constants() about casts and folding. */
